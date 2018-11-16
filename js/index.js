e775a143df14fd4f2d66ffcc0d30f8c4b5fad691
Naomi Dennis <njdennis94@gmail.com>
2018-11-16 16:54:44 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,54 @@
 // your code here
+// describe('displayCommits', () => {
+//       it('parses and displays json values', () => {
+//         var resp = { responseText: commitsData() }
+//         displayCommits.call(resp)
+//         el = document.getElementById("details")
+//         expect(el.innerHTML).toMatch(/Monalisa Octocat/)
+//         expect(el.innerHTML).toMatch(/octocat/)
+//         expect(el.innerHTML).toMatch(/Fix all the bugs/)
+//       })
+//     })
+
+
+
+function displayCommits(){
+  let response = JSON.parse(this.responseText)
+  let details = ""
+  document.querySelector("#details").innerHTML = ""
+  document.querySelector("#details").innerHTML += "<h3>Commit Messages</h3><ul>"
+  for(const itr of response){
+      details = `<li>${itr.commit.message}</li><ul><li>${itr.commit.author.name}</li><li>${itr.committer.login}</li></ul>`
+      document.querySelector("#details").innerHTML += details
+  }
+  document.querySelector("#details").innerHTML += "</ul>"
+}
+function getBranches(){
+  
+}
+
+
+function getCommits(repo_url, repo_name){ 
+  const commit_req = new XMLHttpRequest()
+  commit_req.addEventListener("load", displayCommits)
+  commit_req.open("GET", repo_url)
+  commit_req.send(); 
+}
+
+function checkData(){
+  let response = JSON.parse(this.responseText)
+  let code = "<ul>"
+  document.querySelector("#repositories").innerHTML = ""
+  for(const itr of response){
+    document.querySelector("#repositories").innerHTML += `${itr.name}<ul><li><a style="text-decoration:underline;color:blue;cursor:pointer" onclick='getCommits("${itr.url}/commits", "${itr.name}"); return true;'>View Repository</a></li><li>View Branches</li></ul>`
+  }
+  code += "</ul>"
+}
+
+function getRepositories(){
+  let username = document.querySelector("input").value
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", checkData)
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
